c5cd510a6a9061aa40e0e5312e63c34120ef5462
Kristof Feys <kristof.feys@gmail.com>
2022-01-31 13:05:58 +0100

    add vue

---
+++ src/data.js
@@ -0,0 +1,130 @@
+const data = [
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/koen@2x.png',
+        name: 'Koen Floré',
+        title: 'Managing Partner - Operations',
+        phone: '+32 9 274 79 00',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/wim@2x.png',
+        name: 'Wim Floré',
+        title: 'Managing Partner - Partner development & Finance',
+        phone: '+32 9 274 79 01',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/tom@2x.png',
+        name: 'Tom Maes',
+        title: 'Managing partner - Business development',
+        phone: '+32 9 274 79 02',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/05/Janghys-1.png',
+        name: 'Jan Ghys',
+        title: 'Bike Lease Expert',
+        phone: '+32 473 20 00 00',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/10/Sebastien.png',
+        name: 'Sebastien Vercammen',
+        title: 'Bike Lease Expert',
+        phone: '+32 9 274 79 09',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2020/06/Sanne.png',
+        name: 'Sanne De Meyere',
+        title: 'Bike Lease Expert',
+        phone: '+32 9 274 79 10',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/sarah@2x.png',
+        name: 'Sarah D’hondt',
+        title: 'Onboarding & Customer success',
+        phone: '+32 9 274 79 05',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/04/Jeroen.png',
+        name: 'Jeroen Schalenbourg',
+        title: 'Customer success',
+        phone: '+32 9 274 79 17',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/02/Jenni.png',
+        name: 'Jenni Nollet',
+        title: 'Digital Marketer',
+        phone: '+32 9 274 79 11',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/wes@2x.png',
+        name: 'Wes Depoorter',
+        title: 'Chief Product Officer & Creative Designer',
+        phone: '+32 9 274 79 04',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/04/Stefanie.png',
+        name: 'Stefanie Van Trappen',
+        title: 'Content Creative & Translations',
+        phone: '+32 9 274 79 18',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2020/05/Foto_jan.png',
+        name: 'Jan Seynnaeve',
+        title: 'Order Processing & Customer support',
+        phone: '+32 9 274 79 12',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/01/Kevin.png',
+        name: 'Kevin Deconinck',
+        title: 'Order Processing & Customer support',
+        phone: '+32 9 274 79 14',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/jak@2x.png',
+        name: 'Jakobus De Vriendt',
+        title: 'Processor Services & Accounting',
+        phone: '+32 9 274 79 03',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/09/wimj@2x.png',
+        name: 'Wim Janin',
+        title: 'Chief Technology Officer',
+        phone: '+32 496 55 85 05',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2019/11/nicky@2x.png',
+        name: 'Nicky De Schoesitter',
+        title: 'Software Tester',
+        phone: '+32 496 231 671',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2020/10/Nico.png',
+        name: 'Nico Van Cleemput',
+        title: 'Back-end Developer',
+        phone: '+32 9 274 79 08',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/08/GEERT.png',
+        name: 'Geert De Rycke',
+        title: 'Chief Financial Officer',
+        phone: '+32 478 96 25 85',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/10/Tycho.png',
+        name: 'Tycho Verbelen',
+        title: 'Customer Succes',
+        phone: '+32 496 231 671',
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/12/Joyce.png',
+        name: 'Joyce Van Duyvenboden',
+        title: 'Order processing',
+        phone: null,
+    },
+    {
+        img: 'https://www.o2o.be/app/uploads/2021/10/Logo-o2o.png',
+        name: 'Sales Team',
+        title: '',
+        phone: '',
+    }
+];
+
+export default data;
