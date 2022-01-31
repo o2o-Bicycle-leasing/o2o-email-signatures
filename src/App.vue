9663d6899c7da89a8443ae893a08c6141ca32ba2
Kristof Feys <kristof.feys@gmail.com>
2022-01-31 14:21:12 +0100

    layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <div v-for="(person, i) in data" :key="i">
-      <div style = "display: table;width: 100%;">
+      <div style="display: table;width: 100%;">
         <table width="600px" role="presentation" cellspacing="0" cellpadding="0" border="0" align="left">
           <tbody>
           <tr>
             <td style="height:112px;direction:ltr;font-size:0px;text-align:center;padding:0px;background-color:#C5ECC1 !important;">
-              <img :src="person.img" height="88" width="88" alt="" style="border:none;display:block;outline:none;height:88px;width:88px;padding-left:8px;object-fit: cover;border-radius: 50%;" class="CToWUd">
+              <img :src="person.img" height="88" width="88" alt=""
+                   style="border:none;display:block;outline:none;height:88px;width:88px;padding-left:8px;object-fit: cover;border-radius: 50%;"
+                   class="CToWUd">
             </td>
             <td style="width:375px;height:112px;direction:ltr;font-size:0px;padding:0px;vertical-align:top;background-color:#C5ECC1;color:#336650 !important">
               <table width="100%" role="presentation" cellspacing="0" cellpadding="0" border="0">
@@ -17,16 +19,16 @@
                       <tbody>
                       <tr>
                         <td align="left" style="padding:0px;word-break:break-word">
-                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:14pt;font-weight:bold;line-height:1.55;">
-                                                        <font color="#336650"><b>{{ person.name }}</b></font>
-                                                    </span>
+                          <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:14pt;font-weight:bold;line-height:1.55;">
+                            <font color="#336650"><b>{{ person.name }}</b></font>
+                          </span>
                         </td>
                       </tr>
                       <tr>
                         <td align="left" style="padding:0px 0px 4px;word-break:break-word">
-                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:10pt;line-height:1.55;">
-                                                        <font color="#336650">{{ person.title }}</font>
-                                                    </span>
+                          <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:10pt;line-height:1.55;">
+                              <font color="#336650">{{ person.title }}</font>
+                          </span>
                         </td>
                       </tr>
                       <tr align="left" style="padding:0px;word-break:break-word">
@@ -35,36 +37,36 @@
                             <tbody>
                             <tr>
                               <td style="width:34px">
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <span style="color:#336650 !important">Direct:</span>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                  <span style="color:#336650 !important">Direct:</span>
+                                </span>
                               </td>
                               <td style="width:140px">
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <a v-if="person.phone" :href="hrefify(person.phone)">{{ person.phone }}</a>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                  <a v-if="person.phone" :href="hrefify(person.phone)">{{ person.phone }}</a>
+                                </span>
                               </td>
                               <td>
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <a href="https://www.o2o.be/">www.o2o.be</a>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                    <a href="https://www.o2o.be/">www.o2o.be</a>
+                                </span>
                               </td>
                             </tr>
                             <tr>
                               <td style="width:34px">
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <span style="color:#336650 !important">Alg.:</span>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                  <span style="color:#336650 !important">Alg.:</span>
+                                </span>
                               </td>
                               <td style="width:140px">
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <a href="tel:+3292964012">+32 9 296 40 12</a>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                    <a href="tel:+3292964012">+32 9 296 40 12</a>
+                                </span>
                               </td>
                               <td>
-                                                                    <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
-                                                                        <a href="https://www.google.be/maps/place/o2o+-+Company+bike+lease+-+We+make+you(r)+ride+easier/@51.0887261,3.7418109,17z/data=!3m1!4b1!4m5!3m4!1s0x47c376b6afac42a9:0xc962d1354b36a8bd!8m2!3d51.0887269!4d3.7440127">Farmanstraat 40, 9000 Gent</a>
-                                                                    </span>
+                                <span style="font-family:Verdana,Helvetica,Arial,sans-serif;font-size:7pt;">
+                                    <a href="https://www.google.be/maps/place/o2o+-+Company+bike+lease+-+We+make+you(r)+ride+easier/@51.0887261,3.7418109,17z/data=!3m1!4b1!4m5!3m4!1s0x47c376b6afac42a9:0xc962d1354b36a8bd!8m2!3d51.0887269!4d3.7440127">Farmanstraat 40, 9000 Gent</a>
+                                </span>
                               </td>
                             </tr>
                             </tbody>
@@ -85,6 +87,7 @@
           <tr>
             <td colspan="3">
               <a href="https://www.o2o.be/nl/fietsnieuws/deloitte-technology-fast-50-8ste-editie/">
+                <!-- "o2o komt als beste uit de bus in Deloitte's 2021 Technology Fast 50, de lijst van de vijftig snelstgroeiende techonolgiebedrijven in België" -->
                 <img src="https://www.o2o.be/app/uploads/2021/11/Email-Signature-o2o-Company-Bike-LeaseWINNER.png" height="112" width="600" alt="" style="border:none;display:block;outline:none;height:auto;width:600px;" class="CToWUd">
               </a>
             </td>
@@ -100,19 +103,20 @@
 </template>
 
 <script>
-  import data from "./data.js";
-  export default {
-    name: 'App',
-    data: function () {
-      console.log(data);
-      return {
-        data: data,
-      }
-    },
-    methods: {
-      hrefify(phone) {
-        return 'tel:' + phone.replace(' ','');
-      }
+import data from "./data.js";
+
+export default {
+  name: 'App',
+  data: function () {
+    console.log(data);
+    return {
+      data: data,
+    }
+  },
+  methods: {
+    hrefify(phone) {
+      return 'tel:' + phone.replace(' ', '');
     }
   }
+}
 </script>
